967ef8c94dce9f05e7afc0e86e3fcccb90f0816c
Daniela Castro <someonealiveh@gmail.com>
2025-04-06 13:16:34 +0100

    extra cosmetic scripts to help polish the scene view

---
+++ SimToolAI-Unity/Assets/Scripts/Examples/Unity/Cosmetic/CustomCursor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Examples.Unity.Cosmetic
+{
+    public class CustomCursor: MonoBehaviour
+    {
+        [SerializeField] private Texture2D cursorTexture;
+        [SerializeField] private Texture2D cursorTextureOnClick;
+
+        private void Start()
+        {
+            Cursor.SetCursor(cursorTexture, Vector3.zero, CursorMode.ForceSoftware);
+        }
+    }
+}
---
+++ SimToolAI-Unity/Assets/Scripts/Examples/Unity/Cosmetic/PlayerAnimations.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Examples.Unity.Cosmetic
+{
+    public class PlayerAnimations: MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        
+        public const string SHOOT_ANIMATION = "Shoot";
+        
+        public void TriggerAnimation(string animationName)
+        {
+            animator.SetTrigger(animationName);
+        }
+    }
+}
