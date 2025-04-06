1716cac6035b5319f527d32e7cd44894b9536e22
Daniela Castro <someonealiveh@gmail.com>
2025-04-06 13:09:50 +0100

    create base classes

--- SimToolAI/Core/Map/ISimMap.cs
+++ SimToolAI/Core/Map/ISimMap.cs
@@ -1,55 +1,131 @@
-﻿using SimToolAI.Core.Entities;
+using System;
+using SimToolAI.Core.Entities;
 using SimToolAI.Core.Rendering;
+using SimToolAI.Utilities;
 
 namespace SimToolAI.Core.Map
 {
     /// <summary>
-    /// Interface for all maps in the game.
+    /// Interface for all maps in the simulation
     /// </summary>
     public interface ISimMap
     {
-        public int Width { get; }
-        public int Height { get; }
-        public IRenderable Renderable { get; }
-    
+        /// <summary>
+        /// Gets the width of the map
+        /// </summary>
+        int Width { get; }
+
+        /// <summary>
+        /// Gets the height of the map
+        /// </summary>
+        int Height { get; }
+
+        /// <summary>
+        /// Gets the renderable for the map
+        /// </summary>
+        IRenderable Renderable { get; }
+
+        /// <summary>
+        /// Initializes the map with a renderable
+        /// </summary>
+        /// <param name="renderable">Renderable for the map</param>
+        void Initialize(IRenderable renderable);
+
+        /// <summary>
+        /// Checks if a position is within the map bounds
+        /// </summary>
+        /// <param name="x">X-coordinate</param>
+        /// <param name="y">Y-coordinate</param>
+        /// <returns>True if the position is within the map bounds, false otherwise</returns>
+        bool IsInBounds(int x, int y);
+
         /// <summary>
         /// Checks if a position is walkable
         /// </summary>
-        public bool IsWalkable(int x, int y);
-    
+        /// <param name="x">X-coordinate</param>
+        /// <param name="y">Y-coordinate</param>
+        /// <returns>True if the position is walkable, false otherwise</returns>
+        bool IsWalkable(int x, int y);
+
         /// <summary>
         /// Checks if a position is transparent (for line of sight)
         /// </summary>
-        public bool IsTransparent(int x, int y);
-    
+        /// <param name="x">X-coordinate</param>
+        /// <param name="y">Y-coordinate</param>
+        /// <returns>True if the position is transparent, false otherwise</returns>
+        bool IsTransparent(int x, int y);
+
         /// <summary>
         /// Sets the walkable property of a cell
         /// </summary>
-        public void SetWalkable(int x, int y, bool isWalkable);
-    
+        /// <param name="x">X-coordinate</param>
+        /// <param name="y">Y-coordinate</param>
+        /// <param name="isWalkable">Whether the cell should be walkable</param>
+        void SetWalkable(int x, int y, bool isWalkable);
+
         /// <summary>
         /// Sets the transparent property of a cell
         /// </summary>
-        public void SetTransparent(int x, int y, bool isTransparent);
+        /// <param name="x">X-coordinate</param>
+        /// <param name="y">Y-coordinate</param>
+        /// <param name="isTransparent">Whether the cell should be transparent</param>
+        void SetTransparent(int x, int y, bool isTransparent);
 
         /// <summary>
-        /// Computes the field of view from a given position
+        /// Computes the field of view from a given entity's position
         /// </summary>
-        public void ComputeFov(Entity entity, bool lightWalls = true);
-    
+        /// <param name="entity">Entity to compute field of view for</param>
+        /// <param name="lightWalls">Whether walls should be visible at the edge of the field of view</param>
+        void ComputeFov(Entity entity, bool lightWalls = true);
+
+        /// <summary>
+        /// Toggles field of view computation for an entity
+        /// </summary>
+        /// <param name="entity">Entity to toggle field of view for</param>
+        /// <param name="enabled">Whether field of view should be enabled</param>
+        void ToggleFieldOfView(Entity entity, bool enabled = true);
+
         /// <summary>
         /// Checks if a position is in the current field of view
         /// </summary>
-        public bool IsInFov(int x, int y);
-    
+        /// <param name="x">X-coordinate</param>
+        /// <param name="y">Y-coordinate</param>
+        /// <returns>True if the position is in the field of view, false otherwise</returns>
+        bool IsInFov(int x, int y);
+
         /// <summary>
         /// Attempts to set an entity's position on the map
         /// </summary>
-        public bool SetEntityPosition(Entity entity, int x, int y);
-    
+        /// <param name="entity">Entity to move</param>
+        /// <param name="x">X-coordinate</param>
+        /// <param name="y">Y-coordinate</param>
+        /// <returns>True if the entity was moved, false otherwise</returns>
+        bool SetEntityPosition(Entity entity, int x, int y);
+
         /// <summary>
         /// Gets a random walkable location in a specified area
         /// </summary>
-        public (int x, int y)? GetRandomWalkableLocation(int minX, int maxX, int minY, int maxY);
+        /// <param name="minX">Minimum X-coordinate</param>
+        /// <param name="maxX">Maximum X-coordinate</param>
+        /// <param name="minY">Minimum Y-coordinate</param>
+        /// <param name="maxY">Maximum Y-coordinate</param>
+        /// <returns>A random walkable location, or null if none was found</returns>
+        (int x, int y)? GetRandomWalkableLocation(int minX, int maxX, int minY, int maxY);
+
+        /// <summary>
+        /// Gets a random walkable location anywhere on the map
+        /// </summary>
+        /// <returns>A random walkable location, or null if none was found</returns>
+        (int x, int y)? GetRandomWalkableLocation();
+
+        /// <summary>
+        /// Gets the distance between two positions
+        /// </summary>
+        /// <param name="x1">X-coordinate of the first position</param>
+        /// <param name="y1">Y-coordinate of the first position</param>
+        /// <param name="x2">X-coordinate of the second position</param>
+        /// <param name="y2">Y-coordinate of the second position</param>
+        /// <returns>The distance between the two positions</returns>
+        float GetDistance(int x1, int y1, int x2, int y2);
     }
 }
--- SimToolAI/Core/Rendering/IRenderable.cs
+++ SimToolAI/Core/Rendering/IRenderable.cs
@@ -1,17 +1,31 @@
-﻿using SimToolAI.Utilities;
+using SimToolAI.Utilities;
 
 namespace SimToolAI.Core.Rendering
 {
+    /// <summary>
+    /// Interface for objects that can be rendered
+    /// Implements the Strategy pattern for rendering
+    /// </summary>
     public interface IRenderable
     {
         /// <summary>
-        /// Initialize the renderer
-        /// </summary>
-        //void Initialize(Data arguments);
-
-        /// <summary>
-        /// Present the rendered frame
+        /// Renders the object to the current rendering context
         /// </summary>
         void Render();
+
+        /// <summary>
+        /// Gets whether this object needs to be rendered
+        /// </summary>
+        bool NeedsRendering { get; }
+
+        /// <summary>
+        /// Marks this object as needing to be rendered
+        /// </summary>
+        void MarkForRendering();
+
+        /// <summary>
+        /// Gets the rendering priority (higher values are rendered later/on top)
+        /// </summary>
+        int RenderPriority { get; }
     }
 }
